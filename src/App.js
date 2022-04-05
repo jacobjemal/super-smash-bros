4e26439191ae842ef38e101fc8b070fc07f9c9ea
jacob jemal <jacobjemal7@gmail.com>
2022-04-05 13:25:36 -0400

    created fighters screen components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Fighter from './components/Fighter';
+import FighterScreen from './components/FighterScreen';
 
 function App() {
   return (
@@ -11,13 +12,7 @@ function App() {
         <Fighter name={'Ganondorf'} color={'purple'} />
         <Fighter name={'Sheik'} color={'lightskyblue'} />
       </div>
-      <div>
-        <h2>Roy</h2>
-        <p>
-        <img src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />         
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusantium delectus enim asperiores blanditiis magni numquam. Accusamus minus debitis ipsum ad quibusdam et odio voluptate culpa similique, esse iusto ut!
-          </p>
-      </div>
+      <FighterScreen /> 
     </div>
   );
 }
---
+++ src/components/FighterScreen.js
@@ -0,0 +1,12 @@
+const FighterScreen = () =>{
+    return(
+        <div className="fighter-screen">
+        <h2>Roy</h2>
+        <p>
+        <img width='100' height='100' src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />         
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusantium delectus enim asperiores blanditiis magni numquam. Accusamus minus debitis ipsum ad quibusdam et odio voluptate culpa similique, esse iusto ut!
+        </p>
+      </div>
+    )
+}
+export default FighterScreen
