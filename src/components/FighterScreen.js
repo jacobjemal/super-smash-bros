bc8a343bd9ab6bb0e9860ee1c2d149ff0584edb6
jacob jemal <jacobjemal7@gmail.com>
2022-04-05 17:55:44 -0400

    fixed fighters img

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -5,7 +5,7 @@ const FighterScreen = ({isVisible, setVisible, slectedCharacter}) => {
         style={{
             display: isVisible ? "block" : "none" }}>
         <h2>{slectedCharacter.name}</h2>
-        <img width='100' height='100' src={`"https://www.smashbros.com/assets_v2/img/fighter/${slectedCharacter.name.toLowerCase()}/main.png"`} />         
+        <img width='100' height='100' src={`https://www.smashbros.com/assets_v2/img/fighter/${slectedCharacter.name.toLowerCase()}/main.png`} />         
         <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusantium delectus enim asperiores blanditiis magni numquam. Accusamus minus debitis ipsum ad quibusdam et odio voluptate culpa similique, esse iusto ut!
         </p>
