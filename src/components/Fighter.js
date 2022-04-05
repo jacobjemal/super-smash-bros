31d6679e518a8f80be8fb1a93671f66719950bba
jacob jemal <jacobjemal7@gmail.com>
2022-04-05 17:53:56 -0400

    added . map

--- src/components/Fighter.js
+++ src/components/Fighter.js
@@ -1,8 +1,11 @@
 
-export default  ({ name, color, setVisible }) => {
+export default  ({ name, color, setVisible, setSlectedCharacter }) => {
     return(
         <div 
-        onClick={() => {setVisible(true)}}
+        onClick={() => {
+            setSlectedCharacter({name: name, color: color})
+            setVisible(true)
+        }}
         className="fighter" 
         style={{
             backgroundColor: color, 
--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -1,13 +1,15 @@
-const FighterScreen = ({isVisible, setVisible}) => {
+const FighterScreen = ({isVisible, setVisible, slectedCharacter}) => {
     return(
         <div onClick={() => {setVisible(false)}}
-        className="fighter-screen" style={{display: isVisible ? "block" : "none" }}>
-        <h2>Roy</h2>
+        className="fighter-screen" 
+        style={{
+            display: isVisible ? "block" : "none" }}>
+        <h2>{slectedCharacter.name}</h2>
+        <img width='100' height='100' src={`"https://www.smashbros.com/assets_v2/img/fighter/${slectedCharacter.name.toLowerCase()}/main.png"`} />         
         <p>
-        <img width='100' height='100' src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />         
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusantium delectus enim asperiores blanditiis magni numquam. Accusamus minus debitis ipsum ad quibusdam et odio voluptate culpa similique, esse iusto ut!
         </p>
-    s</div>
+    </div>
     )
 }
 export default FighterScreen
